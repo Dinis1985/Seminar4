67f4710523dfd656ee379022cad38eea15285f45
Dinis <Dinis_abd@example.com>
2022-08-15 22:47:17 +0300

    add work

---
+++ Task26/Program.cs
@@ -0,0 +1,78 @@
+﻿// Задача 26: Напишите программу, которая принимает
+// на вход число и выдаёт количество цифр в числе.
+// 456 -> 3
+// 78 -> 2
+// 89126 -> 5
+
+int CountNum(int number);
+{
+    int i = 1;
+
+    while (number % 10 != number)
+    {
+        number = number / 10;
+        i++;
+    }
+    return i;
+}
+
+Console.WriteLine("Введите число: ");
+int num = Convert.ToUInt32(Console.ReadLine());
+
+int result = CountNum(num);
+Console.WriteLine(result);
+
+
+// Console.Write(" Введите число: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+// int x = num;
+// int count = 0;
+// if (x == 0)
+//     {
+//         count = 1;
+//     }
+// else
+// {
+// while (x != 0)
+//     {
+//         x = x / 10;
+//         count++;
+//     }
+// }
+
+// Вариант
+// int Numbers(int number)
+// {
+// int x = number;
+//  if (x == 0)
+//     {
+//         count = 1;
+//     }
+// else
+// {
+// while (x != 0)
+//     {
+//         x = x / 10;
+//         count++;
+//     }
+// }  
+// return count;
+// }
+// Console.WriteLine(Numbers(num));
+
+
+
+
+
+// //Вариант 2
+// // Console.WriteLine("Введите число: ");
+// // int num = Convert.ToUInt32(Console.ReadLine());
+
+// // int i = 1;
+
+// // while (num % 10! = num)
+// // {
+// //     num = num / 10;
+// //     i++;
+// // }
+// // Concole.WriteLine(i);
---
+++ Task30/Program.cs
@@ -0,0 +1,36 @@
+﻿// Задача 30: Напишите программу, которая
+// выводит массив из 8 элементов, заполненный
+// нулями и единицами в случайном порядке.
+// [1,0,1,1,0,1,0,0]
+
+
+// Задача 30: Напишите программу, которая
+// выводит массив из 8 элементов, заполненный
+// нулями и единицами в случайном порядке.
+// [1,0,1,1,0,1,0,0]
+// int number = new Random().Next(100, 1000);
+
+void MasRandom(int[] array)
+{
+    Random rmd = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = rmd.Next(0, 2);
+    }
+}
+
+void ShowArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    { 
+        if (i < array.Length-1) Console.Write(array[i] + ",");
+        else Console.Write(array[i]);
+    }
+    Console.WriteLine("]");
+}
+
+Console.WriteLine("Вывод массива:");
+int[] mas = new int[8];
+MasRandom(mas);
+ShowArray(mas);
