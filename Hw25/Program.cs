d0f6efadb3a4720da3f421045f4decd6be630b51
Dinis <Dinis_abd@example.com>
2022-08-22 16:49:43 +0300

    25

--- Hw25/Program.cs
+++ Hw25/Program.cs
@@ -3,3 +3,16 @@
 // Пример:
 // 3, 5 -> 243 (3⁵)
 // 2, 4 -> 16
+
+Console.Write("Введите число A: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число B: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+int result = 1;
+for (int i = 1; i <= b; i++)
+{
+    result = result * a;
+}
+
+Console.WriteLine($"Число {a} в натуральной степени {b} -> {result}");
